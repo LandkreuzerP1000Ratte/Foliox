1ab3dce4f78b630e5d0d3dabb7d67a8c2be6eae5
Landkreuzer <j3058885@gmail.com>
2026-08-17 14:37:40 +0200

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("java")
+}
+
+group = "de.landkreuzer"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.formdev:flatlaf:3.7.2")
+}
